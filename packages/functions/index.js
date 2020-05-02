7ad96f17f63ac57cc37d1b56970d39a1b9ebb708
Andrew Ash <andrew@emotiveapps.com>
2020-05-03 01:19:11 +0300

    Test code to send an email

--- packages/functions/index.js
+++ packages/functions/index.js
@@ -1,8 +1,26 @@
 const functions = require('firebase-functions');
+const mailgun = require('mailgun-js');
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+  response.send('Hello from Firebase!');
 });
+
+// Sends an email confirmation when a subscribes for updates
+exports.sendEmailConfirmation = functions.https.onRequest(
+  (request, response) => {
+    const domain = 'mg.remoteshiva.org';
+    const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain });
+    const data = {
+      from: 'Excited User <me@samples.mailgun.org>',
+      to: 'enderash@gmail.com',
+      subject: 'Hello',
+      text: 'Testing some Mailgun awesomness!',
+    };
+    mg.messages().send(data, function (error, body) {
+      response.send(body);
+    });
+  }
+);
