93c634747480bceeba7e83365f6f2641fef45e80
Andrew Ash <andrew@emotiveapps.com>
2020-05-03 03:43:18 +0300

    full implementation of exports.sendEarlyAdopterConfirmation doesn’t work - make it simpler

--- packages/functions/index.js
+++ packages/functions/index.js
@@ -9,18 +9,67 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 });
 
 // Sends an email confirmation when a subscribes for updates
-exports.sendEmailConfirmation = functions.https.onRequest(
-  (request, response) => {
+exports.sendEarlyAdopterConfirmation = functions.firestore
+  .document('earlyadopters/{email}')
+  .onWrite((change, context) => {
+    if (
+      !change.after.exists ||
+      change.after.data().earlyAdopterConfirmationStatus !== undefined
+    ) {
+      // do nothing if the earlyadopter was deleted or earlyAdopterConfirmationSent is already set
+      return;
+    }
+    const toEmail = context.params.email;
+    const toFullName = change.after.data().fullName;
     const domain = 'mg.remoteshiva.org';
     const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain });
-    const data = {
-      from: 'Excited User <me@samples.mailgun.org>',
-      to: 'enderash@gmail.com',
-      subject: 'Hello',
-      text: 'Testing some Mailgun awesomness!',
+
+    // 1 - Add user to mailing list for future use
+    const list = mailgun.lists(`earlyadopters@${domain}`);
+    const newMember = {
+      subscribed: true,
+      address: toEmail,
+      name: toFullName,
+      vars: { isRabbiOrLeader: change.after.data().isRabbiOrLeader },
     };
-    mg.messages().send(data, function (error, body) {
-      response.send(body);
+    list.members().create(newMember, (error, data) => {
+      if (!error) {
+        // 2 - Send confirmation email to user
+        // Mailgun Documentation: https://documentation.mailgun.com/en/latest/user_manual.html#templates
+        const data = {
+          from: 'RemoteShiva <info@remoteshiva.org>',
+          to: toEmail,
+          subject: 'Thank you from RemoteShiva',
+          template: 'send_early_adopter_confirmation',
+          'h:X-Mailgun-Variables': `{"fullName": "${toFullName}", "email": "${toEmail}"}`,
+        };
+        mg.messages().send(data, function (error2, body) {
+          if (!error) {
+            return change.after.ref.set(
+              {
+                earlyAdopterConfirmationStatus: 'sent',
+                addedToMailingListStatus: true,
+              },
+              { merge: true }
+            );
+          }
+          return change.after.ref.set(
+            {
+              earlyAdopterConfirmationStatus: `mailgun error ${error}`,
+              addedToMailingListStatus: true,
+            },
+            { merge: true }
+          );
+        });
+      } else {
+        // could not add them to mailing list
+        return change.after.ref.set(
+          {
+            earlyAdopterConfirmationStatus: `mailgun error ${error}`,
+            addedToMailingListStatus: false,
+          },
+          { merge: true }
+        );
+      }
     });
-  }
-);
+  });
