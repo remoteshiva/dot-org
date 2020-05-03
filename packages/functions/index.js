26d402ff976c35a53cfdf7ebe95d190a7d11cf65
Andrew Ash <andrew@emotiveapps.com>
2020-05-03 03:53:27 +0300

    Simpler implementation of this function

--- packages/functions/index.js
+++ packages/functions/index.js
@@ -11,65 +11,35 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
 // Sends an email confirmation when a subscribes for updates
 exports.sendEarlyAdopterConfirmation = functions.firestore
   .document('earlyadopters/{email}')
-  .onWrite((change, context) => {
-    if (
-      !change.after.exists ||
-      change.after.data().earlyAdopterConfirmationStatus !== undefined
-    ) {
-      // do nothing if the earlyadopter was deleted or earlyAdopterConfirmationSent is already set
-      return;
-    }
+  .onCreate((snap, context) => {
     const toEmail = context.params.email;
-    const toFullName = change.after.data().fullName;
+    const toFullName = snap.data().fullName;
+
     const domain = 'mg.remoteshiva.org';
     const mg = mailgun({ apiKey: process.env.MAILGUN_API_KEY, domain });
 
-    // 1 - Add user to mailing list for future use
-    const list = mailgun.lists(`earlyadopters@${domain}`);
-    const newMember = {
-      subscribed: true,
-      address: toEmail,
-      name: toFullName,
-      vars: { isRabbiOrLeader: change.after.data().isRabbiOrLeader },
+    // Mailgun Documentation: https://documentation.mailgun.com/en/latest/user_manual.html#templates
+    const data = {
+      from: 'RemoteShiva <info@remoteshiva.org>',
+      to: toEmail,
+      subject: 'Thank you from RemoteShiva',
+      template: 'send_early_adopter_confirmation',
+      'h:X-Mailgun-Variables': `{"fullName": "${toFullName}", "email": "${toEmail}"}`,
     };
-    list.members().create(newMember, (error, data) => {
+    mg.messages().send(data, function (error, body) {
       if (!error) {
-        // 2 - Send confirmation email to user
-        // Mailgun Documentation: https://documentation.mailgun.com/en/latest/user_manual.html#templates
-        const data = {
-          from: 'RemoteShiva <info@remoteshiva.org>',
-          to: toEmail,
-          subject: 'Thank you from RemoteShiva',
-          template: 'send_early_adopter_confirmation',
-          'h:X-Mailgun-Variables': `{"fullName": "${toFullName}", "email": "${toEmail}"}`,
-        };
-        mg.messages().send(data, function (error2, body) {
-          if (!error) {
-            return change.after.ref.set(
-              {
-                earlyAdopterConfirmationStatus: 'sent',
-                addedToMailingListStatus: true,
-              },
-              { merge: true }
-            );
-          }
-          return change.after.ref.set(
-            {
-              earlyAdopterConfirmationStatus: `mailgun error ${error}`,
-              addedToMailingListStatus: true,
-            },
-            { merge: true }
-          );
-        });
-      } else {
-        // could not add them to mailing list
-        return change.after.ref.set(
+        return snap.ref.set(
           {
-            earlyAdopterConfirmationStatus: `mailgun error ${error}`,
-            addedToMailingListStatus: false,
+            earlyAdopterConfirmationStatus: 'sent',
           },
           { merge: true }
         );
       }
+      return snap.ref.set(
+        {
+          earlyAdopterConfirmationStatus: `mailgun error ${error}`,
+        },
+        { merge: true }
+      );
     });
   });
