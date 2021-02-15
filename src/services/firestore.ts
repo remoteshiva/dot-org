b78d572e31f04087266048a280ae1b6c99d2bded
Andrew Ash <andrewash@allspice.attlocal.net>
2021-02-14 21:32:47 -0600

    Add Firebase analytics

--- src/services/firestore.ts
+++ src/services/firestore.ts
@@ -16,10 +16,13 @@ const firebaseConfig = {
   projectId: process.env.FIREBASE_PROJECT_ID,
   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
+const analytics = firebase.analytics();
 
 export const authenticateAnonymously = () => {
   return firebase.auth().signInAnonymously();
