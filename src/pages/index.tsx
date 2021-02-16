9cbb72bf35378448673e2d824eea1af8e59c678f
Andrew Ash <andrewash@allspice.local>
2021-02-16 16:21:16 -0600

    Analytics should finally be working, oy

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import firebase from 'firebase';
+import amplitude from 'amplitude-js';
 import Navigation from '../components/molecules/Navigation';
 import { theme } from '../assets/theme';
 
 export default function App() {
+  const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
+    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  amplitude.getInstance().init(process.env.FIREBASE_AMPLITUDE_KEY);
+  amplitude.getInstance().logEvent(`Site_Opened`);
   return <Navigation theme={theme} />;
 }
